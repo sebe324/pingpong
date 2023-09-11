d7f901c053820aa51838c280d1cf63e4ff80df63
sebe324 <sebeq05@gmail.com>
2023-09-11 09:49:56 +0200

    Small fix Fixed utils.cpp Added cmake

--- utils.cpp
+++ utils.cpp
@@ -1,22 +1,22 @@
 #include "utils.h"
-Utils::isPositive(float number){
+int Utils::isPositive(float number){
 if(number>=0) return 1;
 else return -1;
 }
-Utils::clamp(int minimum, int value, int maximum){
+int Utils::clamp(int minimum, int value, int maximum){
     if(value<=minimum) return minimum;
     else if(value>=maximum) return maximum;
     else return value;
 }
 
-Utils::toHexInt(std::string str){
+int Utils::toHexInt(std::string str){
   int num;
     std::stringstream ss(str);
     ss >> std::hex >> num;
     return num;
 }
 
-Utils::toInt(std::string str){
+int Utils::toInt(std::string str){
   int num;
     std::stringstream ss(str);
     ss >> num;
--- utils.h
+++ utils.h
@@ -3,11 +3,10 @@
 
 #include <sstream>
 
-class Utils{
-    public:
-static int clamp(int minimum, int value, int maximum);
-static int isPositive(float number); //if a number is positive or 0 returns 1, else returns -1
-static int toHexInt(std::string str);
-static int toInt(std::string str);
+namespace Utils{
+int clamp(int minimum, int value, int maximum);
+int isPositive(float number); //if a number is positive or 0 returns 1, else returns -1
+int toHexInt(std::string str);
+int toInt(std::string str);
 };
 #endif
